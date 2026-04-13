2ff94132831a28be0217d06f77f199554cbf9d72
Alexis Poveda <59886539+aspoveda95@users.noreply.github.com>
2026-04-12 23:16:59 -0500

    chore: Nx workspace, docker-compose, shared Python requirements

---
+++ apps/api-core/nexus_api/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config para api-core."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nexus_api.settings")
+
+application = get_wsgi_application()
